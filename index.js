62ee2d95ee03813aac94dfb2cd751ce70ed37a8f
unknown <bykorotkov@gmail.com>
2023-07-27 15:52:50 +0400

    request moved out from plugin + small refactor changes

--- index.js
+++ index.js
@@ -21,29 +21,29 @@
 
         return {
             show: function () {
-                loader.show();
-                setTimeout(function () {
-                    loader.hide();
-
-                    $.ajax({
-                        url: "https://jsonplaceholder.typicode.com/users",
-                        method: "GET",
-                        success: function (data) {
-                            console.log(data);
-                            // Можно обработать данные здесь и использовать их по своему усмотрению
-                        },
-                        error: function (error) {
-                            console.log("Произошла ошибка при загрузке данных:", error);
-                        },
-                        complete: function () {
-                            loader.hide();
-                        },
+                // Добавил здесь проверку на существование loader, также добавил параметр скорости (300)
+                if (!loader.length) {
+                    loader = $("<div>").addClass("spg-loader");
+                    loader.css({
+                        "background-color": settings.backgroundColor,
+                        opacity: settings.opacity,
                     });
-                    console.log("Все супер, мы загрузили данные по юзерам");
-                }, 3000);
+
+                    var spinner = $("<div>").addClass("spg-spinner");
+                    loader.append(spinner);
+
+                    this.append(loader);
+                }
+
+                loader.fadeIn(300);
             },
+            // А тут добавил к fadeOut() колбэк чтобы убедиться, что анимация завершена перед тем, как скрыть элемент, и проверку на существование лоадера
             hide: function () {
-                loader.hide();
+                loader.fadeOut(function () {
+                    if ($(this).length) {
+                        $(this).remove();
+                    }
+                });
             },
         };
     };
@@ -57,5 +57,23 @@ $(function () {
 
     $("#button").click(function () {
         loader.show();
+        setTimeout(function () {
+            // loader.hide();
+
+            $.ajax({
+                url: "https://jsonplaceholder.typicode.com/users",
+                method: "GET",
+                success: function (data) {
+                    console.log(data);
+                    // Можно обработать данные здесь и использовать их по своему усмотрению
+                },
+                error: function (error) {
+                    console.log("Произошла ошибка при загрузке данных:", error);
+                },
+                complete: function () {
+                    loader.hide();
+                },
+            });
+        }, 3000);
     });
 });
